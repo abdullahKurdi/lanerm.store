bb7294412f9329b6ffe152f1652be8cdcd8014fb
Abdullah Alkurdi <kurdi313@gmail.com>
2024-12-14 01:56:46 +0200

    lanerm

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,17 +15,17 @@ class DatabaseSeeder extends Seeder
     {
 //        $this->call(WorldSeeder::class);
 //        $this->call(WorldStatusSeeder::class);
-        $this->call(EntrustSeeder::class);
-        $this->call(UserAddressSeeder::class);
-//        $this->call(ProductCategorySeeder::class);
-//        $this->call(TagSeeder::class);
-//        $this->call(ProductSeeder::class);
-//        $this->call(ProductsTagsSeeder::class);
-//        $this->call(ProductsImagesSeeder::class);
-//        $this->call(ProductCouponSeeder::class);
-//        $this->call(ProductReviewSeeder::class);
-        $this->call(ShippingCompanySeeder::class);
-        $this->call(PaymentMethodSeeder::class);
+//        $this->call(EntrustSeeder::class);
+//        $this->call(UserAddressSeeder::class);
+        $this->call(ProductCategorySeeder::class);
+        $this->call(TagSeeder::class);
+        $this->call(ProductSeeder::class);
+        $this->call(ProductsTagsSeeder::class);
+        $this->call(ProductsImagesSeeder::class);
+        $this->call(ProductCouponSeeder::class);
+        $this->call(ProductReviewSeeder::class);
+//        $this->call(ShippingCompanySeeder::class);
+//        $this->call(PaymentMethodSeeder::class);
 //        $this->call(OrderSeeder::class);
 
     }
